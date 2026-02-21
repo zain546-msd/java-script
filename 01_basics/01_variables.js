deb3e432fb334e42d8bb65372dffbdca4b5d2e06
Zain Ullah <117561101+zain546-msd@users.noreply.github.com>
2026-02-21 01:56:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 11223;
+let accountEmail = "zain@gmail.com"
+var accountPasword = "12345";
+accountcity = "Islamabad";
+
+//accountId = 2;
+console.log(accountId);
+console.log("zain");
+
