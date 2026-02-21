ea63856305209d0a66d80e30ad2ce2ba2953dc6b
Zain Ullah <117561101+zain546-msd@users.noreply.github.com>
2026-02-21 01:49:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) //we are using node js not browser
+
+console.log(3
+          +3) //code readability should be high
+
+console.log("zain")
+let name = "zain"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to 56
+// bigint
+// string = ""
+// boolean => true/false
+// null => stand alone value
+// undefined
+// symbol => unique
+// object
+
+
+console.log(typeof undefined) //type undefined
+console.log(typeof null) //type object
